ee08c224df19cee6f461afbc2d8e687fea3ade96
jumgal23 <120317950+jumgal23@users.noreply.github.com>
2023-12-09 14:01:29 +0600

    создал супер пользователя

--- source/book/admin.py
+++ source/book/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from book.models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'name', 'email', 'text', 'created_at', 'updated_at']
+    list_display_links = ['id', 'status', 'name', 'email']
+    list_filter = ['status', 'name']
+    search_fields = ['id', 'status', 'name']
+    fields = ['status', 'name', 'email', 'text']
+    readonly_fields = ['created_at', 'updated_at']
